498d214ec66083654ab46551e159020a0f93acaa
Lexin Gong <gonglexin@gmail.com>
2019-10-28 11:47:31 +0800

    Upgrade to phoenix 1.4.10

--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
-defmodule FatmanGo.Mixfile do
+defmodule FatmanGo.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :fatman_go,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -32,14 +32,13 @@ defmodule FatmanGo.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
+      {:phoenix, "~> 1.4.10"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.0", runtime: false},
       {:observer_cli, "~> 1.4"}
     ]
   end
